22ea87c077ace37b2e702bb0635fb59e6682e046
Pavel <user080902@gmail.com>
2023-09-19 14:35:56 +0300

    test git

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String args[]){
+        int volume = 1600;
+        int fillingSpeed = 30; //30 litres per minute
+        int devastationSpeed = 10; //10 litres per minute
+        int pool = 0;
+        int perSecond = 0;
+
+        while(true){
+            if(pool <= volume){
+                pool = pool + fillingSpeed - devastationSpeed;
+                perSecond ++;
+            } else {
+                System.out.print(perSecond + "seconds");
+                break;
+            }
+
+        }
+
+
+
+
+    }
 }
